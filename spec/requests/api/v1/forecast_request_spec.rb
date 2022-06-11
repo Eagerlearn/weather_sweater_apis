98159a7b3f3f18a9c130ceead88c508b6492c08d
craig cardwell <wizeduvcc@gmail.com>
2022-06-11 14:00:27 -0600

    Initial commit

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Forecast API" do
+  it "sends a list of details for a forecast" do
+    create_list(:forecast, 3)
+
+    get '/api/v1/forecast'
+
+    expect(response).to be_successful
+  end
+end
