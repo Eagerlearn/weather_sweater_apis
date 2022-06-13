97743bd594278d64cef687177c02ad0c2bfe4b00
craig cardwell <wizeduvcc@gmail.com>
2022-06-13 10:32:36 -0600

    wip edit booksearch service and review api docs to confirm correct endpoint

--- app/services/book_search_service.rb
+++ app/services/book_search_service.rb
@@ -1,10 +1,14 @@
 class BookSearchService
   def self.conn
-    Faraday.new('http://openlibrary.org') do |faraday|
-    end
+  x = Faraday.new('http://openlibrary.org', headers: {'Content-Type' => 'application/json'})
+      require "pry"; binding.pry
   end
 
   def self.find_books_by_params(location, quantity)
-
+    response = conn.get('/subjects') do |faraday|
+      faraday.params[:location] = location
+      faraday.params[:quantity] = quantity
+    end
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
