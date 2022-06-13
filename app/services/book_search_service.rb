3009adfdcf6b1784cb125993f9028c60693cfc1b
craig cardwell <wizeduvcc@gmail.com>
2022-06-13 11:01:35 -0600

    wip still unable to connect to endpoint, will revisit

--- app/services/book_search_service.rb
+++ app/services/book_search_service.rb
@@ -1,13 +1,15 @@
 class BookSearchService
   def self.conn
-  x = Faraday.new('http://openlibrary.org', headers: {'Content-Type' => 'application/json'})
-      require "pry"; binding.pry
+    Faraday.new('http://openlibrary.org')
   end
 
   def self.find_books_by_params(location, quantity)
-    response = conn.get('/subjects') do |faraday|
-      faraday.params[:location] = location
-      faraday.params[:quantity] = quantity
+    response = conn.get('/subjects/', headers: {'Content-Type' => 'application/json'}) do |faraday|
+      faraday.params[:name] = location
+      faraday.params[:work_count] = quantity
+      # require "pry"; binding.pry
+      # getting nil returned for response, connection appears to work, but not reaching endpoint
+      # attempted multiple ways to convert to json (assumed that was the issue) unsuccessfully
     end
     JSON.parse(response.body, symbolize_names: true)
   end
