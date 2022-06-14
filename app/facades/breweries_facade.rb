7f69f296a5e61db1ede6bcbdca5b51805a859274
craig cardwell <wizeduvcc@gmail.com>
2022-06-14 11:01:02 -0600

    wip poros

--- app/facades/breweries_facade.rb
+++ app/facades/breweries_facade.rb
@@ -1,10 +1,9 @@
 class BreweriesFacade
   def self.get_breweries_list(location, quantity)
     breweries = BreweriesService.breweries_search(location, quantity)
+    # require "pry"; binding.pry
     # brewery_objects = breweries.map do |brewery|
     #   Brewery.new(brewery)
     # end
-    
-    require "pry"; binding.pry
   end
 end
--- app/services/breweries_service.rb
+++ app/services/breweries_service.rb
@@ -3,20 +3,20 @@ class BreweriesService < BaseService
     city_coordinates = mapquest_conn.get('/geocoding/v1/address') do |faraday|
       faraday.params['location'] = location
     end
-    json = JSON.parse(city_coordinates.body, symbolize_names: true)
+    co_ord = JSON.parse(city_coordinates.body, symbolize_names: true)
 
-    lat = json[:results][0][:locations][0][:latLng][:lat]
-    lon = json[:results][0][:locations][0][:latLng][:lng]
+    lat = co_ord[:results][0][:locations][0][:latLng][:lat]
+    lon = co_ord[:results][0][:locations][0][:latLng][:lng]
 
-    city_forecast = open_weather_connection.get('/data/2.5/onecall') do |faraday|
+    city = open_weather_connection.get('/data/2.5/onecall') do |faraday|
       faraday.params['appid'] = ENV['OW_API_KEY']
       faraday.params['lon'] = lon
       faraday.params['lat'] = lat
       faraday.params['units'] = 'imperial'
     end
-    JSON.parse(city_forecast.body, symbolize_names: true)
+    forecast = JSON.parse(city_forecast.body, symbolize_names: true)
 
-    response = open_brewery_conn.get('/breweries') do |faraday|
+    breweries = open_brewery_conn.get('/breweries') do |faraday|
       faraday.params['by_city'] = location
       faraday.params['per_page'] = quantity
     end
