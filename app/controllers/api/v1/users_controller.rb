a39d2c5c1306f67779b454b2928f343002e2d2e2
craig cardwell <wizeduvcc@gmail.com>
2022-06-15 15:35:14 -0600

    feat: add user registration post request controller action model validations, migration for api_key and serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   def registration
-    registered_user = User.new
+    key = {api_key: SecureRandom.hex(13)}
+    registered_user = User.create!(user_params.merge(key))
+
+    render json: UsersSerializer.serialized_user(registered_user), status: 201
+  end
+
+  private
+  def user_params
+    params.permit(:email, :password, :api_key, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password
+  validates_presence_of :api_key
+  validates :api_key, uniqueness: true
   has_secure_password
+
+  # def self.create_api_key
+  #   SecureRandom.hex(13)
+  # end
+
+  # before_save do
+  #   User.create_api_key
+  # end
 end
