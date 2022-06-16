5a66931da6836872b5f5e762f307d6091ba64506
craig cardwell <wizeduvcc@gmail.com>
2022-06-16 02:37:03 -0600

    edit forecast facade weather arrival method due to nil value error, add road trip poro

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,14 +1,14 @@
 class RoadTripFacade
   def self.get_roadtrip_details(origin, destination)
 
-    trip_route = MapquestGeoService.get_trip_directions(origin, destination)
-    city_weather = ForecastService.get_city_forecast(origin)
-    coordinates = MapquestGeoService.get_coordinates(destination)
+    # trip_route = MapquestGeoService.get_trip_directions(origin, destination)
+    # city_weather = ForecastService.get_city_forecast(origin)
+    # coordinates = MapquestGeoService.get_coordinates(destination)
     duration = MapquestGeoFacade.calculate_travel_time(origin, destination)
     unless duration == "Impossible Route"
       weather = ForecastFacade.weather_at_arrival(destination, duration)
     end
-    RoadTrip.new(origin, destination, weather)
+    RoadTrip.new(origin, destination, duration, weather)
   end
 end
 
---
+++ app/poros/road_trip.rb
@@ -0,0 +1,27 @@
+class RoadTrip
+
+  attr_reader :start_city,
+              :end_city,
+              :travel_time,
+              :weather_at_eta
+
+  def initialize(origin, destination, duration, weather)
+      @start_city     = origin
+      @end_city       = destination
+      @travel_time    = duration
+      @weather_at_eta = nested_weather(weather)
+      # require "pry"; binding.pry
+  end
+
+  def nested_weather(weather)
+    if @travel_time == 'Impossible Route'
+      {}
+    else
+      @travel_time = @travel_time[:formatted_time]
+      {
+        temperature: weather[:temp],
+        conditions: weather[:weather][0][:description]
+      }
+    end
+  end
+end
