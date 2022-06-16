6a2e93f7a39ebcb5098c3d24f0d2bb3fd9a54d5f
craig cardwell <wizeduvcc@gmail.com>
2022-06-16 03:29:35 -0600

    add working controller and serializer for road trip

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::RoadTripsController < ApplicationController
   def create
+    registered_user = User.find_by(api_key: roadtrip_params[:api_key])
+
+    registered_user.present? && params[:origin].present? && params[:destination].present?
     road_trip = RoadTripFacade.get_roadtrip_details(params[:origin], params[:destination])
+
+    render json: RoadTripSerializer.serialized_road_trip(road_trip), status: 201
+    # key = {api_key: SecureRandom.hex(13)}
+    # actual_road_trip = RoadTrip.create!(roadtrip_params.merge(key))
+
+    # require "pry"; binding.pry
+  end
+
+  def roadtrip_params
+    params.permit(:origin, :destination, :api_key)
   end
 end
---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,19 @@
+class RoadTripSerializer
+  def self.serialized_road_trip(road_trip)
+    {
+      "data": {
+        "id": nil,
+        "type": "roadtrip",
+        "attributes": {
+          "start_city": road_trip.start_city,
+          "end_city": road_trip.end_city,
+          "travel_time": road_trip.travel_time,
+          "weather_at_eta": {
+            "temperature": road_trip.weather_at_eta[:temperature],
+            "conditions": road_trip.weather_at_eta[:conditions]
+          }
+        }
+      }
+    }
+  end
+end
