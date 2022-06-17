35288ee3841021405a97685a69e0e2fbbf8b977e
craig cardwell <wizeduvcc@gmail.com>
2022-06-16 23:21:18 -0600

    add sad path for impossible route and missing API key

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,21 +1,26 @@
 class Api::V1::RoadTripsController < ApplicationController
   def create
-    registered_user = User.find_by(api_key: roadtrip_params[:api_key])
+    registered_user = User.find_by!(api_key: params[:api_key]) if params[:api_key] != nil
 
-    if registered_user.present? && params[:origin].present? && params[:destination].present?
-      road_trip = RoadTripFacade.get_roadtrip_details(params[:origin], params[:destination])
+    if registered_user.present?
+        if params[:origin].present? && params[:destination].present?
+          road_trip = RoadTripFacade.get_roadtrip_details(params[:origin], params[:destination])
 
-      render json: RoadTripSerializer.serialized_road_trip(road_trip), status: 201
+          render json: RoadTripSerializer.serialized_road_trip(road_trip), status: 201
+        end
     else
-      render json: { error: "Impossible Route" }, status: 402
+      render json: { error: "Unauthorized" }, status: 401
     end
+  end
+
     # key = {api_key: SecureRandom.hex(13)}
     # actual_road_trip = RoadTrip.create!(roadtrip_params.merge(key))
 
     # require "pry"; binding.pry
-  end
-
+  private
   def roadtrip_params
     params.permit(:origin, :destination, :api_key)
   end
 end
+
+# && params[:origin].present? && params[:destination].present?
